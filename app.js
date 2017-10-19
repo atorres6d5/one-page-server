b457573e02e727e681d7ff3fd3fdaccb71aa5a10
Aldo Torres <atorres6d5@gmail.com>
2017-10-19 10:44:01 -0700

    yes

--- app.js
+++ app.js
@@ -2,5 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-const listener = () => console.log(`Application listening on port ${port}!`)
+const listener = () => console.log(`Party on Garth ${port}!`)
 app.listen(port, listener)
+
+app.get('/ping', (req, res) => {
+  const message = 'pong!'
+  console.log(`ON THE SERVER: ${message}`)
+  res.setHeader('content-type', 'text/html');
+  res.send(`<h1>Now you're cooking with oil!</h1>`)
+})
